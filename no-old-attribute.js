ed7e44ea63f623caf743271aa845aba29b72eea2
jstpierre2906 <jstpierre2906@outlook.com>
2024-11-09 14:32:11 -0500

    got it working great, some cleanup

--- no-old-attribute.js
+++ no-old-attribute.js
@@ -1,44 +1,41 @@
-const { parseForESLint } = require("@html-eslint/parser");
-
+const setMeta = () => ({
+  type: "problem",
+  docs: {
+    description: "disallow old HTML attributes and replace bgcolor",
+    category: "Best Practices",
+    recommended: false,
+  },
+  fixable: "code",
+  schema: [], // no options
+});
+const fromPredicate = (attr) => {
+  return (
+    attr.key.type === "AttributeKey" &&
+    attr.key.value === "bgcolor" &&
+    attr.value.type === "AttributeValue" &&
+    attr.value.value === "turquoise"
+  );
+};
+const callFixerFunc = (fixer, attr) => {
+  const start = attr.range[0];
+  const end = attr.range[1];
+  return fixer.replaceTextRange([start, end], 'class="turquoise"');
+};
+const setReport = (attr) => ({
+  node: attr,
+  message: "Old HTML attribute 'bgcolor' is not allowed. Use 'class' instead.",
+  fix: (fixer) => callFixerFunc(fixer, attr),
+});
 module.exports = {
-  meta: {
-    type: "problem",
-    docs: {
-      description: "disallow old HTML attributes and replace bgcolor",
-      category: "Best Practices",
-      recommended: false,
+  meta: setMeta(),
+  create: (context) => ({
+    Tag(node) {
+      if (!node?.attributes.length) {
+        return;
+      }
+      node.attributes
+        .filter((attr) => fromPredicate(attr))
+        .forEach((attr) => context.report(setReport(attr)));
     },
-    fixable: "code",
-    schema: [], // no options
-  },
-  create: function (context) {
-    return {
-      Program(node) {
-        const html = context.getSourceCode().text;
-        const { ast } = parseForESLint(html);
-
-        // console.log(ast.body)
-        ast.body
-          .filter((b) => b.type === "Tag")
-          .forEach((b) => {
-            // console.log(b);
-            // console.log(b.attributes);
-            b.attributes
-              .filter((attr) => attr.key.type === "AttributeKey" && attr.key.value === "bgcolor")
-              .forEach((attr) => {
-                console.log(attr);
-                context.report({
-                  node: attr,
-                  message: "Old HTML attribute 'bgcolor' is not allowed. Use 'class' instead.",
-                  fix: (fixer) => {
-                    const start = attr.range[0];
-                    const end = attr.range[1];
-                    return fixer.replaceTextRange([start, end], 'class="turquoise"');
-                  },
-                });
-              });
-          });
-      },
-    };
-  },
+  }),
 };
